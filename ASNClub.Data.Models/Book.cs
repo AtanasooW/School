0df86e6bb24b97531db05d70cacdf3b74f61717d
Anton <tonkata2007@gmail.com>
2024-06-07 09:22:15 +0300

    Added new prop

--- ASNClub.Data.Models/Book.cs
+++ ASNClub.Data.Models/Book.cs
@@ -6,7 +6,11 @@ namespace School.Data.Models
     {
         [Key]
         public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
         public string Author { get; set; } = null!;
+        [Required]
         public string Category { get; set; } = null!;
         public ICollection<BookUser> Rents { get; set; } = new HashSet<BookUser>();
     }
