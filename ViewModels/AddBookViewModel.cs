7f456da4b2164f6739144153e75d667b19308ed6
Anton <tonkata2007@gmail.com>
2024-06-07 09:24:23 +0300

    Added ViewModels

---
+++ ViewModels/AddBookViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ViewModels
+{
+    public class AddBookViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Category { get; set; } = null!;
+    }
+}
---
+++ ViewModels/BookDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.ViewModels
+{
+    public class BookDetailsViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Category { get; set; } = null!;
+    }
+}
