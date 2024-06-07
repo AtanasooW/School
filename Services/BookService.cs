ba83a9e3566f95c8dc3e865472fdc4420714fd3e
Anton <tonkata2007@gmail.com>
2024-06-07 09:22:49 +0300

    Adding Add and Details logic

---
+++ Services/BookService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Data;
+using School.Data.Models;
+using School.ViewModels;
+using ViewModels;
+namespace Services
+{
+    public class BookService
+    {
+        private readonly SchoolContext db;
+
+        public BookService(SchoolContext context)
+        {
+            db = context;
+        }
+        public async Task<AddBookViewModel> GetAddBookViewModelAsync()
+        {
+            return new AddBookViewModel();
+        }
+        public async Task AddBook(AddBookViewModel model)
+        {
+            var book = new Book
+            {
+                Id = model.Id,
+                Author = model.Author,
+                Title = model.Title,
+                Category = model.Category,
+            };
+            await db.AddAsync(book);
+            await db.SaveChangesAsync();
+        }
+        public async Task<BookDetailsViewModel?> GetBookByIdAsync(Guid Id)
+        {
+            var book = await db.Books.Where(x => x.Id == Id).Select(x => new BookDetailsViewModel
+            {
+                Id = x.Id,
+                Author = x.Author,
+                Title = x.Title,
+                Category = x.Category,
+            }).FirstOrDefaultAsync();
+            return book;
+
+        }
+    }
+}
