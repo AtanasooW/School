5803d40e39cc570de655047b7111c9b484697d07
Anton <tonkata2007@gmail.com>
2024-06-11 19:05:52 +0300

    Implementing interface library

---
+++ Contracts/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using School.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IBookService
+    {
+        Task<AddBookViewModel> GetAddBookViewModelAsync();
+        Task AddBook(AddBookViewModel model);
+        Task<BookDetailsViewModel?> GetBookByIdAsync(Guid Id);
+    }
+}
